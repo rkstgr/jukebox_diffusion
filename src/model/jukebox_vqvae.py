ee9f44f9fbf48de37915d612a52c2846104ccb7e
Erik Steiger <rkstgr@gmail.com>
2023-02-09 14:49:15 +0100

    Move vqvae to device

--- src/model/jukebox_vqvae.py
+++ src/model/jukebox_vqvae.py
@@ -13,19 +13,18 @@ class JukeboxVQVAEModel:
             vae_path = os.environ["JUKEBOX_VQVAE_PATH"]
             
         print("Loading Jukebox VAE from", vae_path)
-        self.jukebox_vqvae = self.get_vqvae(vae_path, device)
+        self.jukebox_vqvae = self.get_vqvae(vae_path, device).eval().to(device)
 
     def get_vqvae(self, vae_path, device="cpu"):
         config = JukeboxVQVAEConfig.from_pretrained("openai/jukebox-1b-lyrics")
         vae = JukeboxVQVAE(config)
         vae.load_state_dict(torch.load(vae_path, map_location=device))
-        vae.eval().to(device)
         return vae
     
     @torch.no_grad()
     def encode(self, audio, lvl):
         audio = rearrange(audio, "b t c -> b c t")
-        encoder = self.jukebox_vqvae.encoders[lvl]
+        encoder = self.jukebox_vqvae.encoders[lvl].to(audio.device)
         embeddings = encoder(audio)[-1]
         embeddings = rearrange(embeddings, "b c t -> b t c")
         return embeddings
@@ -33,7 +32,7 @@ class JukeboxVQVAEModel:
     @torch.no_grad()
     def decode(self, embeddings, lvl):
         embeddings = rearrange(embeddings, "b t c -> b c t")
-        decoder = self.jukebox_vqvae.decoders[lvl]
+        decoder = self.jukebox_vqvae.decoders[lvl].to(embeddings.device)
         audio = decoder([embeddings], all_levels=False)
         audio = audio.permute(0, 2, 1)
         return audio
