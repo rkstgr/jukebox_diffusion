1ba839e8e04f7bc620ecb578314592b2dddcfedd
Erik Steiger <rkstgr@gmail.com>
2023-02-09 16:44:00 +0100

    Add maestro_all to compute_normalizations

--- scripts/compute_normalization.py
+++ scripts/compute_normalization.py
@@ -13,11 +13,11 @@ datasets = {
     "maestro_val": MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="validation", sample_length=44100*10, aug_shift=False),
     "maestro_train": MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="train", sample_length=44100*10, aug_shift=False),
     "maestro_test": MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="test", sample_length=44100*10, aug_shift=False),
-    
+
     "maestro_all": ConcatDataset([
-        MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="train", sample_length=44100*10, aug_shift=False),
-        MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="validation", sample_length=44100*10, aug_shift=False),
-        MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="test", sample_length=44100*10, aug_shift=False),
+        MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="train", sample_length=44100*20, aug_shift=False),
+        MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="validation", sample_length=44100*20, aug_shift=False),
+        MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="test", sample_length=44100*20, aug_shift=False),
     ]),
 }
 
@@ -34,8 +34,8 @@ def main(dataset_name: str, embedding_lvl: int):
     apply_fn = lambda x: vqvae.encode(x.to(device), lvl=embedding_lvl).detach().to("cpu")
 
     mean, std = normalizer.compute_stats_iter(dataloader, apply_fn=apply_fn, total=len(dataloader))
-    normalizer.save_stats(f"config/normalizations/{dataset_name}_lvl_{embedding_lvl}.pt", (mean, std))
+    normalizer.save_stats(f"{dataset_name}_lvl_{embedding_lvl}.pt", (mean, std))
 
 
 if __name__ == "__main__":
-    main("maestro_train", 1)
+    main("maestro_all", 1)
