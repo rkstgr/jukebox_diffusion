cedd4219d29a8488b633f30288a1dfcc21ea8796
Erik Steiger <rkstgr@gmail.com>
2023-02-24 14:52:36 +0100

    WIP main RDM class

--- src/model/RDM.py
+++ src/model/RDM.py
@@ -71,6 +71,44 @@ class RDMArgs:
     max_token_seq_len: int = 8192
 
 
+class RDMEncoder(nn.Module):
+    def __init__(self, args) -> None:
+        super().__init__()
+
+        self.token_embedding = nn.Embedding(args.token_dict_size + 1, args.encoder_dim, padding_idx=args.mask_idx)
+        self.positional_embedding = IntegerFourierEmbedding(args.encoder_dim, min_index=0, max_index=args.max_token_seq_len)
+
+        self.backbone = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(
+                d_model=args.encoder_dim,
+                nhead=args.encoder_heads,
+                dim_feedforward=args.encoder_ff_dim,
+                dropout=args.encoder_dropout,
+                activation="gelu"
+            ),
+            num_layers=args.encoder_layers,
+            norm=nn.LayerNorm(args.encoder_dim)
+        )
+
+        self.linear = nn.Linear(args.encoder_dim, args.token_dict_size + 1) # music tokens + absorbing state token
+        self.softmax = nn.Softmax(dim=-1)
+
+
+    def forward(self, x_t):
+        """
+        Args:
+            x_t (torch.Tensor): (batch_size, token_seq_len, 2049). Noised/corrupted music tokens one-hot encoded with masked_idx token.
+        Returns:
+            torch.Tensor: (batch_size, token_seq_len, 2049). Softmaxed logits over token ids.
+        """
+        x_t_emb = self.token_embedding(x_t)
+        x_t_emb = x_t_emb + self.positional_embedding(x_t_emb)
+        x_t_emb = self.backbone(x_t_emb)
+        x_t_emb = self.linear(x_t_emb)
+        x_t = self.softmax(x_t_emb)
+        return x_t
+
+
 class RDM(pl.LightningModule):
     def __init__(self, args) -> None:
         super().__init__()
@@ -89,9 +127,6 @@ class RDM(pl.LightningModule):
         )
         self.time_sampler = UniformSampler(args.num_diffusion_timesteps)
 
-        self.token_embedding = nn.Embedding(args.token_dict_size + 1, args.encoder_dim, padding_idx=args.mask_idx)
-        self.positional_embedding = IntegerFourierEmbedding(args.encoder_dim, min_index=0, max_index=args.max_token_seq_len)
-
         self.encoder = nn.TransformerEncoder(
             nn.TransformerEncoderLayer(
                 d_model=args.encoder_dim,
@@ -104,9 +139,6 @@ class RDM(pl.LightningModule):
             norm=nn.LayerNorm(args.encoder_dim)
         )
         
-        self.linear = nn.Linear(args.encoder_dim, args.token_dict_size + 1) # music tokens + absorbing state token
-        self.softmax = nn.Softmax(dim=-1)
-
     def forward(self, x):
         pass
 
@@ -121,51 +153,64 @@ class RDM(pl.LightningModule):
                 -   "t": (batch_size, ). The diffusion time step for each batch.
         """
         x_0 = self.vqvae.encode_quantized(batch)
-        x_0_emb = self.token_embedding(x_0)
+        
+        non_special_sym_mask = x_0.ne(self.vqvae.pad_id)
+
+        if self.args.q_sample_mode == "default":
+            # we use 1 sample for the default sampling trick.
+            num_q_samples = 1            
+        elif self.args.q_sample_mode in ["coupled", "multi-sample", "multi-step"]:
+            # we use 2 samples by default for these advanced sampling tricks,
+            # but feel free to specify as you like.
+            num_q_samples = 2
+            x_0 = x_0.repeat(num_q_samples, 1)
         
 
         batch_size = x_0.shape[0]
         device = x_0.device
+        
         if self.args.q_sample_mode == "coupled":
             t1, weight_t = self.time_sampler.sample(batch_size, device)
             t2, _ = self.time_sampler.sample(batch_size, device)
             x_t, x_0_ignore, mask, t = self.diffusion.q_sample_coupled(x_0=x_0, t1=t1, t2=t2, non_special_sym_mask=non_special_sym_mask) 
             weight_t = weight_t.repeat(num_q_samples)
+
         elif self.args.q_sample_mode == "multi-sample":
             rets = []
-            t, weight_t = self.time_sampler.sample(sample["target"].shape[0], sample["target"].device)
+            t, weight_t = self.time_sampler.sample(batch_size, device)
             for _ in range(num_q_samples):
-                x_t, x_0_ignore, mask = self.diffusion.q_sample(x_0=sample["target"], t=t, non_special_sym_mask=non_special_sym_mask)
+                x_t, x_0_ignore, mask = self.diffusion.q_sample(x_0=x_0, t=t, non_special_sym_mask=non_special_sym_mask)
                 rets.append((t, weight_t, x_t, x_0_ignore, mask))
-            t, weight_t, x_t, x_0_ignore, mask = map(lambda x: torch.cat(x, dim=0), zip(*rets))
+            t, weight_t, x_t, x_0_ignore, mask = map(lambda x: th.cat(x, dim=0), zip(*rets))
+
         elif self.args.q_sample_mode == "multi-step":
             rets = []
             for _ in range(num_q_samples):
-                t, weight_t = self.time_sampler.sample(sample["target"].shape[0], sample["target"].device)
-                x_t, x_0_ignore, mask = self.diffusion.q_sample(x_0=sample["target"], t=t, non_special_sym_mask=non_special_sym_mask)
+                t, weight_t = self.time_sampler.sample(batch_size, device)
+                x_t, x_0_ignore, mask = self.diffusion.q_sample(x_0=x_0, t=t, non_special_sym_mask=non_special_sym_mask)
                 rets.append((t, weight_t, x_t, x_0_ignore, mask))
-            t, weight_t, x_t, x_0_ignore, mask = map(lambda x: torch.cat(x, dim=0), zip(*rets))
+            t, weight_t, x_t, x_0_ignore, mask = map(lambda x: th.cat(x, dim=0), zip(*rets))
         elif self.args.q_sample_mode == "default":
-            t, weight_t = self.time_sampler.sample(sample["target"].shape[0], sample["target"].device)
-            x_t, x_0_ignore, mask = self.diffusion.q_sample(x_0=sample["target"], t=t, non_special_sym_mask=non_special_sym_mask)
-
-        pos_embeddings = self.positional_embedding(tokens)
-        x_0 = tok_embeddings + pos_embeddings
+            t, weight_t = self.time_sampler.sample(batch_size, device)
+            x_t, x_0_ignore, mask = self.diffusion.q_sample(x_0=x_0, t=t, non_special_sym_mask=non_special_sym_mask)
 
         diffusion_dict = {
-            "x_t" : x_t,
-            "x_0_ignore" : x_0_ignore,
-            "masks" : mask,
-            "t": t,
-            "weight_t": weight_t
-        }
+                "x_t" : x_t,
+                "x_0_ignore" : x_0_ignore,
+                "masks" : mask,
+                "t": t,
+                "weight_t": weight_t
+            }
 
+        encoder_out = self.encoder(
+            x_t=diffusion_dict["x_t"],
+            t=diffusion_dict["t"],
+        ) # a tuple ([B, N, C], None) or ([B, N, C], [B, N])
 
-        return {
-            "x_0": batch["x_0"],
-            "x_t": batch["x_t"],
-            "t": batch["t"]
-        }
+        diffusion_dict["decoder_outputs"] = encoder_out
+        diffusion_dict["x_0"] = x_0
+
+        return diffusion_dict
 
     def training_step(self, batch, batch_idx):
         diffusion_dict = self._prepare_batch(batch)
