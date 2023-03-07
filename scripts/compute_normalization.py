535ed9842b2e81d68604ecb7f79783090a8511b8
Erik Steiger <rkstgr@gmail.com>
2023-03-07 10:36:53 +0100

    compute normalizations in memory

--- scripts/compute_normalization.py
+++ scripts/compute_normalization.py
@@ -1,5 +1,8 @@
 import os
+from einops import rearrange
+from tqdm import tqdm
 
+import torch
 from torch.utils.data import DataLoader, ConcatDataset, Subset
 
 from src.datamodule.maestro_datamodule import MaestroDataModule
@@ -12,9 +15,9 @@ from src.model.jukebox_vqvae import JukeboxVQVAEModel
 
 datasets = {
     "maestro": lambda: ConcatDataset([
-        MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="train", sample_length=44100*20, aug_shift=False),
-        MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="validation", sample_length=44100*20, aug_shift=False),
-        MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="test", sample_length=44100*20, aug_shift=False),
+        MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="train", sample_length=44100*5, aug_shift=False),
+        MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="validation", sample_length=44100*5, aug_shift=False),
+        MaestroDataset(os.environ["MAESTRO_DATASET_DIR"], split="test", sample_length=44100*5, aug_shift=False),
     ]),
 
     "acapella": lambda: AcapellaDataset(os.environ["ACAPELLA_DATASET_DIR"], split="all", sample_length=44100*20, aug_shift=False),
@@ -33,9 +36,18 @@ def main(dataset_name: str, embedding_lvl: int, subset: int = 10):
     normalizer = JukeboxNormalizer()
     vqvae = JukeboxVQVAEModel().to(device)
 
-    apply_fn = lambda x: vqvae.encode(x["audio"].to(device), lvl=embedding_lvl).detach().to("cpu")
+    apply_fn = lambda x: vqvae.encode((x["audio"] if isinstance(x, dict) else x).to(device), lvl=embedding_lvl).detach().to("cpu")
 
-    mean, std = normalizer.compute_stats_iter(dataloader, apply_fn=apply_fn, total=len(dataloader))
+    sample_accumulator = []
+    for batch in tqdm(dataloader):
+        sample = apply_fn(batch)
+        sample = rearrange(sample, "b t c -> (b t) c")
+        sample_accumulator.append(sample)
+    
+    sample = torch.cat(sample_accumulator, dim=0)
+    mean = torch.mean(sample, dim=0)
+    std = torch.std(sample, dim=0)
+
     print(mean)
     print(std)
     normalizer.save_stats(f"normalizations/{dataset_name}_lvl_{embedding_lvl}.pt", (mean, std))
@@ -45,7 +57,7 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, default="maestro_train")
+    parser.add_argument("--dataset", type=str, default="maestro")
     parser.add_argument("--embedding_lvl", type=int, default=2)
     parser.add_argument("--subset", type=int, default=10)
     args = parser.parse_args()
